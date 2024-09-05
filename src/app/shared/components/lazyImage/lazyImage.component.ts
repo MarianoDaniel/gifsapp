9ce88e124cbae0e0840f57cf01fe1ba7ae82595a
Mariano Avico <mavico@ignetworks.com>
2024-09-05 19:25:18 -0300

    App Finalizada

---
+++ src/app/shared/components/lazyImage/lazyImage.component.ts
@@ -0,0 +1,22 @@
+import { Component, input, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazyImage.component.html',
+  styleUrl: './lazyImage.component.css',
+})
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL property is required');
+  }
+  onLoad() {
+    this.hasLoaded = true;
+  }
+}
