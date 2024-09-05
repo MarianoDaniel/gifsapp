29efde8b95eea67963a7543eabf41c9be788a615
Mariano Avico <mavico@ignetworks.com>
2024-09-05 14:20:56 -0300

    Antes de abordar la parte de image loader

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -10,7 +10,9 @@ export class GifsService {
     private apiKey: string = 'l4daVfO42KdrQ7cfTKLpN59KPl3gcSmq';
     private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) {
+        this.loadLocalStorage();
+    }
 
     get tagsHistory(): string[] {
 
@@ -26,6 +28,7 @@ export class GifsService {
         }
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this._tagsHistory.splice(0, 10);
+        this.saveLocalStorage();
     }
 
     searchTag(tag: string): void {
@@ -39,8 +42,19 @@ export class GifsService {
         this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
             .subscribe(resp => {
                 this.gifList = resp.data;
-                console.log({ gifs: this.gifList })
             });
+    }
+
+    private saveLocalStorage(): void {
+        localStorage.setItem('history', JSON.stringify(this._tagsHistory))
+    }
+
+    private loadLocalStorage(): void {
+        if (!localStorage.getItem('history')) return;
+        this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+
+        if (this._tagsHistory.length === 0) return;
+        this.searchTag(this._tagsHistory[0]);
     }
 
 }
