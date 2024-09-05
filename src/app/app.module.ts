af39c307a522d9e90457c5a325a2e0606a6ac09d
Mariano Avico <mavico@ignetworks.com>
2024-09-04 23:13:26 -0300

    paramas HttpParams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -14,7 +15,8 @@ import { GifsModule } from './gifs/gifs.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    GifsModule
+    GifsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
 
     private _tagsHistory: string[] = [];
+    private apiKey: string = 'l4daVfO42KdrQ7cfTKLpN59KPl3gcSmq';
+    private serviceUrl: string = 'https://api.giphy.com/v1/gifs'
 
-    constructor() { }
+    constructor(private http: HttpClient) { }
 
     get tagsHistory(): string[] {
 
@@ -13,9 +16,26 @@ export class GifsService {
     }
 
 
-    searchTag(tag: string): void {
+    private organizeHistory(tag: string): void {
+        tag = tag.toLowerCase();
+
+        if (this._tagsHistory.includes(tag)) {
+            this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+        }
         this._tagsHistory.unshift(tag);
+        this._tagsHistory = this._tagsHistory.splice(0, 10);
     }
 
+    searchTag(tag: string): void {
+
+        if (tag.length === 0) return;
+        this.organizeHistory(tag);
+        const params = new HttpParams()
+            .set('api_key', this.apiKey)
+            .set('limit', 10)
+            .set('q', tag)
+        this.http.get(`${this.serviceUrl}/search`, { params })
+            .subscribe(resp => console.log(resp));
+    }
 
 }
